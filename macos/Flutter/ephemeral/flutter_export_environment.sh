b44643ac8380a0b5bcb75a0901db9357ba151ae7
Salar Muhammad Umer <114861875+salarumer@users.noreply.github.com>
2024-08-30 01:24:50 +0500

    Add files via upload

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.22.3/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/salarmuhammadumer/Desktop/Splash_Signup_Screen/splash_signup_screens"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/salarmuhammadumer/Desktop/Splash_Signup_Screen/splash_signup_screens/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8yMzVkYjkxMWJhMjc5NzIyZjVlNjg1ZjM4YjBlZDMwZmE3ZTg1NzBhLw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/salarmuhammadumer/Desktop/Splash_Signup_Screen/splash_signup_screens/.dart_tool/package_config.json"
